7642111565143270901d968be68f1f111e7ed985
ElizaKhanova <hanova_eliza19981007@mail.ru>
2023-01-09 23:53:17 +0200

    Добавление задания со звездочкой

--- task with star/index1.js
+++ task with star/index1.js
@@ -1,9 +1,11 @@
 let year = document.querySelector ('.year').value;
 document.querySelector('button').onclick = Push;
-function Push() {
-if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
-    console.log('Да');
-} else {
-    console.log('Нет');
-}
+
+function Push (){
+    let year = document.querySelector ('.year').value;
+    if ((year%4 == 0) && (year%100 == 0) && (year%400 == 0)) {
+        document.getElementById('answer').value = "Год високосный!";
+    } else {
+        document.getElementById('answer').value = "Год не високосный";
+    }
 }
